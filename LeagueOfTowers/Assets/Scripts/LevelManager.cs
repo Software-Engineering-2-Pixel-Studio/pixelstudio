9aca5f9f4d5f01bc35ca76d51cf8391963337f55
Phamhhuy <56693286+Phamhhuy@users.noreply.github.com>
2021-02-25 04:58:52 -0800

    Setting up PUN2 for Network Manager
    Set up PUN2 network along with menu for create and join game.

--- LeagueOfTowers/Assets/Scripts/LevelManager.cs
+++ LeagueOfTowers/Assets/Scripts/LevelManager.cs
@@ -13,10 +13,14 @@ public class LevelManager : MonoBehaviour
             return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
         }
     }
+    private void Awake()
+    {
+        CreateLevel();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        CreateLevel();
+        
     }
 
     // Update is called once per frame
