fb2a3a487ef1cce73bec3596618cf70bef322246
Hintaycl <35672198+Hintaycl@users.noreply.github.com>
2021-02-25 05:32:42 -0600

    Pre-Tower Placement
    -Created the Grid position script
    -Created Tile Dictionary
    -Created Generics and Singleton
    -Created Tower prefabs
    -Imported assets for the towers

---
+++ LeagueOfTowers/Assets/Scripts/TileScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public Point GridPosition { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup(Point gridPos, Vector3 worldPos, Transform parent){
+        this.GridPosition = gridPos;
+        transform.position = worldPos;
+        transform.SetParent(parent);
+        LevelManager.Instance.Tiles.Add(gridPos, this);
+    }
+}
