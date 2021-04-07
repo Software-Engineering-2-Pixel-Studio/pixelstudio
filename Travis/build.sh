e57bab551339872ea449185254449561a76a44f8
nurida <nurida.karimbaeva.99@gmail.com>
2021-04-07 13:30:32 -0500

    print logs for the unit tests

--- Travis/build.sh
+++ Travis/build.sh
@@ -14,7 +14,7 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-logFile $(pwd)/unity.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
 	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml \
+	-editorTestsResultFile $(pwd)/test.xml
 	-quit
 
 rc0=$?
@@ -25,20 +25,6 @@ if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
-	-quit
-
-rc1=$?
-echo "Build logs (Windows)"
-cat $(pwd)/unity.log
-
 echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
