d53125ed3b2396083cf15b3f26ad8b25aa42622c
nurida <nurida.karimbaeva.99@gmail.com>
2021-04-07 21:41:02 -0500

    build.sh

--- Travis/build.sh
+++ Travis/build.sh
@@ -14,12 +14,12 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-logFile $(pwd)/unity.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
 	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml
+	-editorTestsResultFile $(pwd)/test.xml \
 	-quit
 
 rc0=$?
 echo "Unit test logs"
-#cat $(pwd)/test.xml
+cat $(pwd)/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
