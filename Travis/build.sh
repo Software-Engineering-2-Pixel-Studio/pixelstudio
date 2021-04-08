242c538ffc1d4e769550bd3a2663bcebdc7a0e98
nurida <nurida.karimbaeva.99@gmail.com>
2021-04-08 00:08:48 -0500

    build.sh

--- Travis/build.sh
+++ Travis/build.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
+## Run the editor unit tests
+echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-runEditorTests \
+	-quit
+
+rc0=$?
+echo "Unit test logs"
+
 ## Make the build for the OSX
 echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
