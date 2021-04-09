b4d179541e49d4a60e89228e779c5b0df923486e
nurida <nurida.karimbaeva.99@gmail.com>
2021-04-09 11:12:57 -0500

    Tests for the TowerUpgrade Class

--- LeagueOfTowers/Assets/Editor/TowerUpgradeTests.cs
+++ LeagueOfTowers/Assets/Editor/TowerUpgradeTests.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -12,48 +12,39 @@ public class TowerUpgradeTests
     {
         int price = 10;
         int damage = 15;
-        //float debuffDuration = 1.5;
-        //float debuffProcChance = 0.2;
+        float debuffDuration = 1.5f;
+        float debuffProcChance = 0.2f;
+        float tickTime = 1.3f;
+        int tickDamage = 3;
+        float slowingFactor = 0.4f;
         //create an instance of every type of tower upgrade
         //int price, int damage, float debuffDuration, float debuffProcChance
-        //TowerUpgrade upgradeLight = new TowerUpgrade( price, damage, debuffDuration, debuffProcChance);
+        TowerUpgrade upgradeLight = new TowerUpgrade( price, damage, debuffDuration, debuffProcChance);
         //int price, int damage, float debuffDuration, float debuffProcChance, float tickTime, int tickDamage
-        //TowerUpgrade upgradeFire = new TowerUpgrade( 10, 15, 1.5, 0.2, 1.3, 3);
+        TowerUpgrade upgradeFire = new TowerUpgrade(price, damage, debuffDuration, debuffProcChance, tickTime, tickDamage);
         //int price, int damage, float debuffDuration, float debuffProcChance, float slowingFactor
-        //TowerUpgrade upgradeSlow = new TowerUpgrade( 10, 15, 1.5, 0.2, 0.4);
+        TowerUpgrade upgradeSlow = new TowerUpgrade( price, damage, debuffDuration, debuffProcChance, slowingFactor);
 
         //Check that all of the variables are set properly
         //Light
-        //Assert.AreEqual(upgradeLight.Price, 10);
-/*  
+        Assert.AreEqual(upgradeLight.Price, 10);  
         Assert.AreEqual(upgradeLight.Damage, 15);
-        Assert.AreEqual(upgradeLight.DebuffDuration, 1.5);
-        Assert.AreEqual(upgradeLight.DebuffProcChance, 0.2);
+        Assert.AreEqual(upgradeLight.DebuffDuration, 1.5f);
+        Assert.AreEqual(upgradeLight.DebuffProcChance, 0.2f);
 
         //Fire
         Assert.AreEqual(upgradeFire.Price, 10);
         Assert.AreEqual(upgradeFire.Damage, 15);
-        Assert.AreEqual(upgradeFire.DebuffDuration, 1.5);
-        Assert.AreEqual(upgradeFire.DebuffProcChance, 0.2);
-        Assert.AreEqual(upgradeFire.TickTime, 1.3);
-        Assert.AreEqual(upgradeFire.TickDamage, 3);
+        Assert.AreEqual(upgradeFire.DebuffDuration, 1.5f);
+        Assert.AreEqual(upgradeFire.DebuffProcChance, 0.2f);
+        Assert.AreEqual(upgradeFire.TickTime, 1.3f);
+        Assert.AreEqual(upgradeFire.TickDamage, 3f);
 
         //Slow
         Assert.AreEqual(upgradeSlow.Price, 10);
         Assert.AreEqual(upgradeSlow.Damage, 15);
-        Assert.AreEqual(upgradeSlow.DebuffDuration, 1.5);
-        Assert.AreEqual(upgradeSlow.DebuffProcChance, 0.2);
-        Assert.AreEqual(upgradeSlow.SlowingFactor, 0.4);
-        */
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator TowerUpgradeTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        Assert.AreEqual(upgradeSlow.DebuffDuration, 1.5f);
+        Assert.AreEqual(upgradeSlow.DebuffProcChance, 0.2f);
+        Assert.AreEqual(upgradeSlow.SlowingFactor, 0.4f);
     }
 }
